37a2f888740a0dcb5f1eb7c68718adcaf65e1e83
Adam Perry <adam.n.perry@gmail.com>
2018-05-09 00:39:37 -0700

    include repro case

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate criterion;
+extern crate inflate;
+
+use criterion::{Criterion, black_box};
+use inflate::inflate_bytes;
+
+fn decode(c: &mut Criterion) {
+    c.bench_function("decode", |b| {
+        let compressed = include_bytes!("./1m_random_deflated");
+        b.iter(|| black_box(inflate_bytes(compressed).unwrap()));
+    });
 }
+
+criterion_group!(inflate, decode);
+criterion_main!(inflate);
